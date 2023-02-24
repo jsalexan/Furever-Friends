f566a2f5f46d8013c1153a2b14a6972fac725676
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-02-24 13:27:16 -0800

    Connect to jawsdb

--- public/js/viewProfile.js
+++ public/js/viewProfile.js
@@ -1,16 +1,19 @@
-const viewProfileHandeler = async (event) => {
+const viewProfileHandler = async (event) => {
   event.preventDefault();
 
   const userData = document.querySelector("#viewProfileCard").value.trim();
+
   if (userData) {
     const response = await fetch("/profile/:id", {
         method: 'POST',
         body: JSON.stringify({userData}),
-
+        headers: {
+          'Content-Type': 'application/json',
+        },
     });
 
     if (response.ok) {
-        //document.location.replace('/viewProfile')
+        document.location.replace('/viewProfile')
     } else {
         alert(response.statusText)
     }
