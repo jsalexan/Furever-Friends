1de70e3108dffad6003336f394f6af2b2bfafa2e
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-09-20 17:13:48 -0700

    Was working on adding the likes. Revisit tomorrow. Fixed profile creationg bug. Changed styling on post icon.

--- controllers/api/profile-routes.js
+++ controllers/api/profile-routes.js
@@ -42,4 +42,36 @@ router.get('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProfile = await Profile.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProfile);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const profileData = await Profile.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!profileData) {
+      res.status(404).json({ message: 'No profile found with this id!' });
+      return;
+    }
+
+    res.status(200).json(profileData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
