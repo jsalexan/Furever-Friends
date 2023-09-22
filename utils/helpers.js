09f273699fe90a4ae2e3cd32772a18853866fcff
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-09-22 11:33:33 -0700

    Fixed the helpers to correctly format the date on each comment

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,17 @@
 module.exports = {
-    format_date: (date) => {
-        // Format date as MM/DD/YYYY
-        return date.toLocaleDateString();
-    },
+  format_date: (date) => {
+    if (date) {
+      const originalDate = new Date(date);
+            
+      // Add one day (24 hours) to the date
+      originalDate.setDate(originalDate.getDate() + 1);
+
+      const formattedDate = originalDate.toLocaleDateString();
+      
+      return formattedDate;
+  }
+  return ""; 
+},
     format_plural: (word, amount) => {
         if (amount !== 1) {
           return `${word}s`;
